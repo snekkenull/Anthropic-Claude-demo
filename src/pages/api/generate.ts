d8c50cf4c5da717e9627cbd2c20acab246bbf6d7
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-08 02:30:48 +0800

    remove pass

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -3,32 +3,32 @@
 // #vercel-end
 
 import { APIRoute } from 'astro';
-import { verifySignature } from '@/utils/auth'
+//import { verifySignature } from '@/utils/auth'
 
 const apiKey = import.meta.env.ANTHROPIC_API_KEY
 const apiUrl = ((import.meta.env.ANTHROPIC_API_BASE_URL) || 'https://api.anthropic.com/v1/complete')
 //const apiUrl = 'https://api.anthropic.com/v1/complete';
-const sitePassword = import.meta.env.SITE_PASSWORD || ''
-const passList = sitePassword.split(',') || []
+//const sitePassword = import.meta.env.SITE_PASSWORD || ''
+//const passList = sitePassword.split(',') || []
 
 export const post: APIRoute = async (context) => {
 
   const body = await context.request.json()
 
-  if (sitePassword && !(sitePassword === pass || passList.includes(pass))) {
-    return new Response(JSON.stringify({
-      error: {
-        message: 'Invalid password.',
-      },
-    }), { status: 401 })
-  }
-  if (import.meta.env.PROD && !await verifySignature({ t: time, m: messages?.[messages.length - 1]?.content || '' }, sign)) {
-    return new Response(JSON.stringify({
-      error: {
-        message: 'Invalid signature.',
-      },
-    }), { status: 401 })
-  }
+//  if (sitePassword && !(sitePassword === pass || passList.includes(pass))) {
+//    return new Response(JSON.stringify({
+//      error: {
+//       message: 'Invalid password.',
+//      },
+//    }), { status: 401 })
+//  }
+//  if (import.meta.env.PROD && !await verifySignature({ t: time, m: messages?.[messages.length - 1]?.content || '' }, sign)) {
+//    return new Response(JSON.stringify({
+//      error: {
+//       message: 'Invalid signature.',
+//      },
+//    }), { status: 401 })
+//  }
 
 
   const requestBody = await context.request.json();

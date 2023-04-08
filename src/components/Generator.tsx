0ed6b88373f353316ff6b4ce481aeef1d9fe5dcf
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-08 14:17:28 +0800

    update

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -110,44 +110,39 @@ export default () => {
           }),
         }),
         signal: controller.signal,
-      })
+      });
+  
       if (!response.ok) {
-        const error = await response.json()
-        console.error(error.error)
-        setCurrentError(error.error)
-        throw new Error('Request failed')
+        const error = await response.json();
+        console.error(error.error);
+        setCurrentError(error.error);
+        throw new Error('Request failed');
       }
-      const data = response.body
-      if (!data)
-        throw new Error('No data')
-
-      const reader = data.getReader()
-      const decoder = new TextDecoder('utf-8')
-      let done = false
-
-      while (!done) {
-        const { value, done: readerDone } = await reader.read()
-        if (value) {
-          const char = decoder.decode(value)
-          if (char === '\n' && currentAssistantMessage().endsWith('\n'))
-            continue
-
-          if (char)
-            setCurrentAssistantMessage(currentAssistantMessage() + char)
-
-          isStick() && instantToBottom()
-        }
-        done = readerDone
+  
+      const data = await response.json();
+  
+      if (!data.chat_messages || data.chat_messages.length === 0) {
+        throw new Error('No data');
       }
+  
+      const assistantMessage = data.chat_messages.find(
+        (message: ChatMessage) => message.sender === 'assistant'
+      );
+  
+      if (assistantMessage) {
+        setCurrentAssistantMessage(assistantMessage.text);
+        archiveCurrentMessage();
+        isStick() && instantToBottom();
+      }
+  
     } catch (e) {
-      console.error(e)
-      setLoading(false)
-      setController(null)
-      return
+      console.error(e);
+      setLoading(false);
+      setController(null);
+      return;
     }
-    archiveCurrentMessage()
-    isStick() && instantToBottom()
-  }
+  };
+  
 
   const archiveCurrentMessage = () => {
     if (currentAssistantMessage()) {

2ee535c1931e251ff2320077a25cde192cead02b
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-13 00:25:05 +0800

    update

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -1,5 +1,5 @@
 import { APIRoute } from 'astro';
-import { EventSourcePolyfill as EventSource } from 'event-source-polyfill';
+import fetchStream from 'fetch-readablestream';
 
 const apiKey = import.meta.env.ANTHROPIC_API_KEY;
 const apiUrl = 'https://api.anthropic.com/v1/complete';
@@ -9,7 +9,7 @@ const DONE_MESSAGE = 'DONE';
 export const post: APIRoute = async (context) => {
   const requestBody = await context.request.json();
 
-  const eventSource = new EventSource(`${apiUrl}?stream=true`, {
+  const response = await fetchStream(`${anthropicAPIUrl}/v1/complete?stream=true`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -17,24 +17,27 @@ export const post: APIRoute = async (context) => {
     },
     body: JSON.stringify(requestBody),
   });
+  
 
   const response = new ReadableStream({
     async start(controller) {
-      eventSource.onmessage = (event) => {
-        console.log('Received message:', event.data);
-        if (event.data === DONE_MESSAGE) {
-          controller.close();
-          eventSource.close();
-          return;
-        }
-        controller.enqueue(event.data);
-      };
-
-      eventSource.onerror = (error) => {
-        console.error('Sampling error:', error);
-        controller.error(error);
-        eventSource.close();
-      };
+      response.body
+      .pipeThrough(new TextDecoderStream())
+      .pipeTo(
+        new WritableStream({
+          write(chunk) {
+            console.log('Received message:', chunk);
+            if (chunk === DONE_MESSAGE) {
+              controller.close();
+              return;
+            }
+            controller.enqueue(chunk);
+          },
+          close() {
+            controller.close();
+          },
+        }),
+      );
     },
   });
 

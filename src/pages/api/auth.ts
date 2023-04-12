42af99bc8e6ad98696b136aadf7b4282499fae89
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-12 23:03:51 +0800

    fix errors...

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -1,11 +1,20 @@
 // src/pages/api/auth.ts
 import { APIRoute } from 'astro';
 
-const validPassword = import.meta.env.SITE_PASSWORD;// Replace 'your_password_here' with your desired password.
+const validPassword = import.meta.env.SITE_PASSWORD;
 
 export const post: APIRoute = async (context) => {
   const requestBody = await context.request.json();
   const { pass } = requestBody;
+
+  if (!validPassword) {
+    return new Response(JSON.stringify({ code: 0 }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 
   if (pass === validPassword) {
     return new Response(JSON.stringify({ code: 0 }), {

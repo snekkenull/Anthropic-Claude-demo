9c845284a0c9f2f063748eff47540a11e65e23c3
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-13 15:06:31 +0800

    update

--- src/pages/api/generate.ts
+++ src/pages/api/generate.ts
@@ -6,43 +6,48 @@ const apiUrl = 'https://api.anthropic.com/v1/complete';
 const requestBody = await request.json();
 const messages = requestBody.messages;
 
-const response = await fetch(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`,
-  },
-  body: JSON.stringify({
-    prompt: createPromptFromMessages(messages),
-    stop_sequences: ['\n\nHuman:'],
-    max_tokens_to_sample: 2046,
-    model: 'claude-v1',
-    stream: true,
-  }),
-});
+async function post(request: ServerRequest) {
 
-if (!response.ok) {
-  const error = await response.json();
-  console.error(error.error);
-  return jsonResponse({ error: error.error }, { status: response.status });
-}
+  const requestBody = await request.json();
+  const messages = requestBody.messages;
 
-const decoder = new TextDecoder();
-const streamParser = createParser((event) => {
-  if (event.type === 'event') {
-    try {
-      const json = JSON.parse(event.data);
-      const text = json.completion;
-      request.respond({ body: JSON.stringify({ completion: text }) });
-    } catch (e) {
-      console.error(e);
-    }
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `Bearer ${apiKey}`,
+    },
+    body: JSON.stringify({
+      prompt: createPromptFromMessages(messages),
+      stop_sequences: ['\n\nHuman:'],
+      max_tokens_to_sample: 2046,
+      model: 'claude-v1',
+      stream: true,
+    }),
+  });
+
+  if (!response.ok) {
+    const error = await response.json();
+    console.error(error.error);
+    return jsonResponse({ error: error.error }, { status: response.status });
   }
-});
 
-for await (const chunk of response.body) {
-  streamParser.feed(decoder.decode(chunk));
-}
+  const decoder = new TextDecoder();
+  const streamParser = createParser((event) => {
+    if (event.type === 'event') {
+      try {
+        const json = JSON.parse(event.data);
+        const text = json.completion;
+        request.respond({ body: JSON.stringify({ completion: text }) });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  });
 
-request.finalize();
+  for await (const chunk of response.body) {
+    streamParser.feed(decoder.decode(chunk));
+  }
+
+  request.finalize();
 }

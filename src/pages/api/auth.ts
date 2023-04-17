8426035edfc27fb953cc15e382308ec34e5c5ac9
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-17 15:58:21 +0800

    update

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -7,28 +7,20 @@ export const post: APIRoute = async (context) => {
   const requestBody = await context.request.json();
   const { pass } = requestBody;
 
-  if (!validPassword) {
-    return new Response(JSON.stringify({ code: 0 }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-
-  if (pass === validPassword) {
-    return new Response(JSON.stringify({ code: 0 }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  } else {
-    return new Response(JSON.stringify({ code: 1, message: 'Invalid password' }), {
-      status: 403,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
+  // Remove the check for SITE_PASSWORD
+  // if (pass === validPassword) {
+  return new Response(JSON.stringify({ code: 0 }), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  // } else {
+  //   return new Response(JSON.stringify({ code: 1, message: 'Invalid password' }), {
+  //     status: 403,
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //   });
+  // }
 };

b1cc8aad303fa3ef39c7fc6813f967da8f621abf
snekkenull <124278082+snekkenull@users.noreply.github.com>
2023-04-17 20:10:10 +0800

    update

--- src/pages/api/auth.ts
+++ src/pages/api/auth.ts
@@ -2,29 +2,17 @@ import { APIRoute } from 'astro';
 
 const validPassword = import.meta.env.SITE_PASSWORD;
 
-export const post: APIRoute = async (context) => {
-  const requestBody = await context.request.json();
-  const { pass } = requestBody;
-
+export const get: APIRoute = (context) => {
   if (!validPassword) {
-    return new Response(JSON.stringify({ code: 0 }), {
-      status: 200,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-  }
-
-  if (pass === validPassword) {
-    return new Response(JSON.stringify({ code: 0 }), {
+    return new Response(JSON.stringify({ passwordEnabled: false }), {
       status: 200,
       headers: {
         'Content-Type': 'application/json',
       },
     });
   } else {
-    return new Response(JSON.stringify({ code: 1, message: 'Invalid password' }), {
-      status: 403,
+    return new Response(JSON.stringify({ passwordEnabled: true }), {
+      status: 200,
       headers: {
         'Content-Type': 'application/json',
       },
